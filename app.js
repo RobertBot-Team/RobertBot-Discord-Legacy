143b652507b7c176992b5e87bfcb178283eb1b52
Kleynogue <Kleynogue08@gmail.com>
2026-04-13 00:25:56 -0400

    CAmbio en el puerto de la API

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ import { SlashCommandBuilder } from 'discord.js';
 
 // Create an express app
 const app = express();
-// Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
+// Get port, or default to 80
+const PORT = process.env.PORT || 80;
 // Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
