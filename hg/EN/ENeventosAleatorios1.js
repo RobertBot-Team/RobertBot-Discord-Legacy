e4226e73db29b8711505493792c9f7fd5e2097dd
Kleynogue <Kleynogue08@gmail.com>
2026-05-03 02:42:31 -0400

    Corregido aleatorios 1

--- hg/EN/ENeventosAleatorios1.js
+++ hg/EN/ENeventosAleatorios1.js
@@ -166,6 +166,7 @@ eventosAleatorios1[9] = async (jugador, players, maxHP, teams, req, channel) => 
 //dejo este comentario porque hay que hacer lo mismo en varios eventos
 
 eventosAleatorios1[10] = async (jugador, players, maxHP, teams, req, channel) => {
+  return null;
   let team = jugador.getTeam();
   let cantEnTeam = 1;
   let newTeamId = teams.length + 1;
@@ -404,6 +405,7 @@ eventosAleatorios1[11] = async (jugador, players, maxHP, teams, req, channel) =>
 }
 
 eventosAleatorios1[12] = async (jugador, players, maxHP, teams, req, channel) => {
+  return null;
   let team = jugador.getTeam();
   let vivos = team.nombresVivos();
   let newTeamId = teams.length + 1;
@@ -615,6 +617,7 @@ eventosAleatorios1[13] = async (jugador, players, maxHP, teams, req, channel) =>
 }
 
 eventosAleatorios1[14] = async (jugador, players, maxHP, teams, req, channel) => {
+  return null;
   let idTeamDe2;
   let team;
   let teamActual;
@@ -1542,6 +1545,7 @@ eventosAleatorios1[37] = async (jugador, players, maxHP, teams, req, channel) =>
 }
 
 eventosAleatorios1[38] = async (jugador, players, maxHP, teams, req, channel) => {
+  return null;
   let arraySolos = [];
   let pos1;
   let pos2;
--- hg/eventosAleatorios1.js
+++ hg/eventosAleatorios1.js
@@ -168,6 +168,7 @@ eventosAleatorios1[9] =async (jugador, players, maxHP, teams, req, channel)=>{
     //dejo este comentario porque hay que hacer lo mismo en varios eventos
 
 eventosAleatorios1[10] =async (jugador, players, maxHP, teams, req, channel)=>{
+    return null;
     let team = jugador.getTeam();
     let cantEnTeam = 1;
     let newTeamId = teams.length + 1;
@@ -406,6 +407,7 @@ eventosAleatorios1[11] =async (jugador, players, maxHP, teams, req, channel)=>{
 }
 
 eventosAleatorios1[12] =async (jugador, players, maxHP, teams, req, channel)=>{
+    return null;
     //console.log("\x1b[35m%s\x1b[0m",` this is evento 122222222222222222`);
     let team = jugador.getTeam();
     let vivos = team.nombresVivos();
@@ -618,6 +620,7 @@ eventosAleatorios1[13] =async (jugador, players, maxHP, teams, req, channel)=>{
 }
 
 eventosAleatorios1[14] =async (jugador, players, maxHP, teams, req, channel)=>{
+    return null;
     let idTeamDe2;
     let team;
     let teamActual;
@@ -1538,6 +1541,7 @@ eventosAleatorios1[37] =async (jugador, players, maxHP, teams, req, channel)=>{ 
 }
 
 eventosAleatorios1[38] = async(jugador, players, maxHP, teams, req, channel)=>{
+    return null;
     let arraySolos = [];
     let pos1;
     let pos2;
