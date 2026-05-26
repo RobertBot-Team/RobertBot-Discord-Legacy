ad10bb1893c1806a8815407c91a0f5649ed1fad2
Kleynogue <Kleynogue08@gmail.com>
2026-05-26 01:06:46 -0400

    Agregada explicacion de como jugar

--- controllers/play_i18n.js
+++ controllers/play_i18n.js
@@ -3,7 +3,7 @@ export const SUPPORTED_PLAY_LANGUAGES = ["es", "en"];
 
 const PLAY_TRANSLATIONS = {
   es: {
-    game_started: "Se ha iniciado una nueva partida de Los Juegos del Hambre",
+    game_started: "¿Cómo jugar?\nSimplemente unete dando click en el boton \"Unirse a la batalla\" de abajo. Cuando todos se hayan unido, pueden darle click a \"Comenzar\", y el bot se encargará de todo, solo sientense a mirar y disfrutar del caos. Después de 10 minutos el botón para unirse se desactiva y debes volver a empezar, ¡sé rápido, no quedes fuera!\nHaz click en \"Modo Lento\" si quieres que la partida dure más\n\nSe ha iniciado una nueva partida de Los Juegos del Hambre",
     already_running: "Ya hay una partida en curso",
     wait_timeout: "Se ha acabado el tiempo de espera. Por favor inicia una nueva partida.",
     join_button: "Unirse a la batalla",
@@ -34,7 +34,7 @@ const PLAY_TRANSLATIONS = {
     help_message: `🛠️ **Comandos de RobertBot:**\n\n**/play** - Comenzar una partida nueva de los Juegos del Hambre. Debes seleccionar el idioma de la partida. Es necesario que haya 2 jugadores mínimo para poder iniciar la partida. Hay un máximo de 8 jugadores por partida.\n**/stop** - Detiene la partida en progreso. Solo el jugador que la inició o el owner pueden detenerla.\n**/help** - Muestra este mensaje de ayuda explicando los comandos.`,
   },
   en: {
-    game_started: "A new Hunger Games battle has started",
+    game_started: "How to play?\nSimply join by clicking the \"Join battle\" button below. When everyone has joined, you can click \"Start\", and the bot will take care of everything, just sit back and watch the chaos unfold. After 10 minutes, the joining button will deactivate and you'll have to start again, so be quick, don't miss out!\nClick \"Slow Mode\" if you want the game to last longer.",
     already_running: "There is already a battle in progress",
     wait_timeout: "Waiting time is over. Please start a new battle.",
     join_button: "Join battle",
