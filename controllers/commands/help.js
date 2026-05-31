aa1db7856d376b02999bc91a22715373b7c15e0a
Kleynogue <Kleynogue08@gmail.com>
2026-05-31 14:41:01 -0400

    Corrección de la respuesta de help

--- controllers/commands/help.js
+++ controllers/commands/help.js
@@ -5,14 +5,16 @@ import { EmbedBuilder } from "discord.js";
 export async function help(req, res, language) {
   return res.send({
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    embeds: [
-      new EmbedBuilder()
-        .setColor("Blurple")
-        .setTitle(tPlay(language, "help_embed_title"))
-        .setDescription(tPlay(language, "help_message")),
-      new EmbedBuilder()
-        .setColor("Blurple")
-        .setDescription(tPlay(language, "help_embed_description"))
-    ]
+    data: {
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Blurple")
+          .setTitle(tPlay(language, "help_embed_title"))
+          .setDescription(tPlay(language, "help_message")),
+        new EmbedBuilder()
+          .setColor("Blurple")
+          .setDescription(tPlay(language, "help_embed_description"))
+      ]
+    }
   });
 }
