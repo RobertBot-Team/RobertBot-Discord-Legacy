16d3b9c30407e58393125cc94391689e2850065b
Kleynogue <Kleynogue08@gmail.com>
2026-05-31 03:55:33 -0400

    Otra prueba en el comando help

--- controllers/commands/help.js
+++ controllers/commands/help.js
@@ -4,8 +4,12 @@ import { tPlay } from "../play_i18n.js";
 export async function help(req, res, language) {
   return res.send({
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      content: tPlay(language, "help_message"),
-    }
+    embeds: [
+      new EmbedBuilder()
+        .setTitle(tPlay(language, "help_embed_title"))
+        .setDescription(tPlay(language, "help_message")),
+      new EmbedBuilder()
+        .setDescription(tPlay(language, "help_embed_description"))
+    ]
   });
 }
