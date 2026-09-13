11a3db4ada7ace4066ec567d68e86fb0672d7a63
Kleynogue <Kleynogue08@gmail.com>
2026-09-13 02:12:41 -0400

    Corrección de export en desactivar comando

--- controllers/commands/play.js
+++ controllers/commands/play.js
@@ -573,7 +573,7 @@ function limpiarConstantesYMaps(guildId) {
     clearGameChannelPorGuild(guildId);
 }
 
-async function desactivarComando(channelId, msgid, guildId, client) {
+export async function desactivarComando(channelId, msgid, guildId, client) {
   console.log("Limpiando data desde Timeout...");
   //const messageFetched = await channel.messages.fetch(msgid);
   //console.log(messageFetched.components);
